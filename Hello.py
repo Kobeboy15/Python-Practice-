82a400e30f697dcbca992acf18d7918cbd6a9281
Kobeboy15 <kobemichael15@gmail.com>
2020-02-14 16:21:44 +0800

    I did it mom

---
+++ Hello.py
@@ -0,0 +1,34 @@
+import requests
+import os
+import random
+import string
+import json
+
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+random.seed = (os.urandom(1024))
+
+url = 'http://localhost:8000/signin/'
+
+names = json.loads(open('names.json').read())
+
+isTrue = True
+
+while(isTrue):
+    for name in names:
+        name_extra = ''.join(random.choice(string.digits))
+
+        user = name.lower() + name_extra + '@yahoo.com'
+        passw = ''.join(random.choice(chars) for i in range(8))
+
+        payload = {"data": {'username': user,'password': passw }}
+
+        try:
+            requests.post(url, allow_redirects=False, data=json.dumps(payload))
+            print 'sending username %s and password %s' % (user, passw)
+
+        except:
+            print 'He locked you out'
+            isTrue = False
+            break
+
